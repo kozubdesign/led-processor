0219d5d77f3c04621c61e347e685e8f10901ec8e
Denis Kozub <39241468+kozubdesign@users.noreply.github.com>
2026-04-15 23:12:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -110,8 +110,8 @@ st.markdown(f"""
     /* Размер шрифта разрешения */
     .res-box {{ 
         width: 100%; text-align: center; background-color: #d4edda; color: #155724; 
-        padding: 20px; border-radius: 8px; margin: 15px 0; 
-        font-weight: 500; font-size: 1.4rem;
+        padding: 10px; border-radius: 8px; margin: 15px 0; 
+        font-weight: 500; font-size: 1.0rem;
     }}
     </style>
     <div class="logo-container">
